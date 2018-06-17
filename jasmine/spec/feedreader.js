86b510b710e52729c4172bc27f322f9ed3dbb143
chrismb <36547312+chrismb008@users.noreply.github.com>
2018-06-17 12:36:56 +0300

    Add files via upload

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,18 +1,25 @@
 $(function() {
 
+    /* This suite is all about the RSS feeds definitions, the allFeeds variable in our application.*/
     describe('RSS Feeds', function() {
+
+        /* This test make sure that the allFeeds variable has been defined and that it is not empty.*/
 
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
 
+        /* This test loops through each feed in the allFeeds object and ensures it has a URL defined and that the URL is not empty.*/
+
         it('urls are defined', function() {
             allFeeds.forEach(function(feed) {
               expect(feed.url).toBeDefined();
-              expect(feed.url.lenght).not.toBe(0);
+              expect(feed.url.length).not.toBe(0);
             });
         });
+
+        /* This test loops through each feed in the allFeeds object and ensures it has a name defined and that the name is not empty.*/
 
         it('names are defined', function() {
           allFeeds.forEach(function(feed) {
@@ -22,12 +29,17 @@ $(function() {
         });
     });
 
+    /* This suite is all about the Menu*/
 
     describe('Menu', function() {
+
+        /* This test ensures the menu element is hidden by default.*/
 
         it('menu element is hidden', function() {
             expect($('body').hasClass('menu-hidden')).toBe(true);
         });
+
+        /* This test ensures the menu changes visibility when the menu icon is clicked.*/
 
         it('menu changes visibility', function() {
             $('.menu-icon-link').click();
@@ -37,6 +49,7 @@ $(function() {
         });
     });
 
+    /* This suite is all about the first entries in our feed container*/
 
     describe('Initial Entries', function() {
 
@@ -46,10 +59,14 @@ $(function() {
           });
         });
 
+        /* This test ensures when the loadFeed function is called and completes its work, there is at least a single .entry element within the .feed container.*/
+
         it('there is at least one entry element', function () {
             expect($('.feed .entry').length).toBeGreaterThan(0);
         });
     });
+
+    /* This suite is all about the new entries in our feed container*/
 
     describe('New Feed Selection', function() {
       var entriesFirst,
@@ -65,6 +82,8 @@ $(function() {
             });
           });
         });
+
+        /* This test ensures when a new feed is loaded by the loadFeed function the content actually changes.*/
 
         it('the content changes', function() {
             expect(entriesFirst).not.toBe(entriesLast);
